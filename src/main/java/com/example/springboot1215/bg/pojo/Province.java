622957270fd318465f4a775ebac76be88b15aae0
Xukkai <3215295307@qq.com>
2024-05-30 16:58:56 +0800

    初始代码

---
+++ src/main/java/com/example/springboot1215/bg/pojo/Province.java
@@ -0,0 +1,10 @@
+package com.example.springboot1215.bg.pojo;
+
+import lombok.Data;
+
+@Data
+public class Province {
+    private int id;
+    private String provinceCode;
+    private String provinceName;
+}
